b494136662b3a20585c5d1da68e845cc48b83610
Louis-Luo-Tech <xiangluoapply@gmail.com>
2020-05-07 22:39:31 -0700

    comment and package

--- src/main/scala/com/louis/bigdata/SparkWordCountApp.scala
+++ src/main/scala/com/louis/bigdata/SparkWordCountApp.scala
@@ -4,7 +4,7 @@ import org.apache.spark.{SparkConf, SparkContext}
 
 object SparkWordCountApp {
   def main(args: Array[String]): Unit = {
-    val sparkConf = new SparkConf().setMaster("local[*]").setAppName("SparkWordCountApp")
+    val sparkConf = new SparkConf().setMaster("local[*]").setAppName("SparkWordCountApp")//local mode
     val sc = new SparkContext(sparkConf)
 
     val rdd = sc.textFile("file:///Users/xiangluo/Documents/GitHub/Spark_WordCount/data/input.txt")
